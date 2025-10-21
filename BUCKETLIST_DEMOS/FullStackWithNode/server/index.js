10be2b5b019c03a0b06c21f7d0fe7845158ff8bd
Jon-Mikel <garrisonhgrave@yahoo.com>
2025-10-21 11:12:33 -0500

    added CRud to server

--- BUCKETLIST_DEMOS/FullStackWithNode/server/index.js
+++ BUCKETLIST_DEMOS/FullStackWithNode/server/index.js
@@ -1,2 +1,44 @@
 const {bucketArray} = require("./fakeData")
-console.log(bucketArray)
+const express = require('express')
+// create an instance of server and all the "fun functionality" associated with express
+const app = express()
+const port = process.env.PORT || 3001;
+
+// add body parser
+app.use(express.json())
+// app.use(express.urlencoded({extended: false}))
+
+
+// app.get('/', (req, res)=>{
+//     res.send("Server working")
+// })
+
+app.get('/api/bucket', (req, res)=>{
+    res.json(bucketArray)
+})
+
+let newId = 4
+app.post('/api/bucket', (req, res)=>{
+//     post requests are sent through the response object as "body"
+//     what are we creating
+    console.log(req.body)
+    let newBucketListItem =   {
+        id: newId++,
+        description: req.body.description, // POST request send data through body!!!
+        isComplete: false,
+    }
+    // add to end of current array
+    bucketArray.push(newBucketListItem)
+    let receipt = newBucketListItem
+    res.json(receipt)
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}!`)
+})
+
+
+
+
+
